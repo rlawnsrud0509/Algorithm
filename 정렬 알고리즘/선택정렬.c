bcbbaf0fbd2aefd687fe1241841c610b13beee8a
ddd2341 <rlawnsrud0509@gmail.com>
2023-07-16 22:22:49 +0900

    선택정렬

--- 정렬 알고리즘/선택정렬.c
+++ 정렬 알고리즘/선택정렬.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
+
 	int i, j, min, index, temp;
 	int array[10] = {1, 10, 5, 8, 7, 6, 4, 3, 2, 9};
+	
 	for(i=0; i<10; i++) {
 		min = 9999;
 		for(j=i; j<10; j++) {
